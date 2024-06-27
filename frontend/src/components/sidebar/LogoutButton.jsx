1a81e8885abb1a8356c0d77e71b6ad903867cfe3
Azharuddin <mohdazhar.313@gmail.com>
2024-06-28 00:12:24 +0530

    complete chat app

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,21 @@
+import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
+
+const LogoutButton = () => {
+  const { loading, logout } = useLogout();
+
+  return (
+    <div className="mt-auto flex gap-1">
+      {!loading ? (
+        <BiLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
+      <p className="font-semibold text-white">Logout</p>
+    </div>
+  );
+};
+export default LogoutButton;
